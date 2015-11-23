08c178c93e3f5a73ae959d18822a734a8cdaa6bf
dongw <mr.dong1024@163.com>
2015-11-23 17:13:13 +0800

    chrome插件练习

---
+++ content_script.js
@@ -0,0 +1,90 @@
+﻿
+console.log("Started!");
+
+//html中注入浮动框
+injectDom(document);
+
+chrome.runtime.sendMessage({method: "getLocalStorage", key: "height"}, function(response) {
+  console.log(response.data);
+  var height = response.data; 
+
+	if(!isNaN(height))
+	{
+		console.log("数字");
+		modify(height);
+	}  
+});
+
+
+function modify(newHeight)
+{	
+	{
+		var style = "overflow: auto; width: 100%; height:" + newHeight + "px;"
+		console.log("%s", style);
+    	$(".dataTables_scrollBody").attr("style", style);
+	}
+}
+
+function injectDom(doc){
+	var oDisplay, oStyle, oHeader, oSwithcer, oScore;
+
+    oDisplay = doc.createElement("div");
+    oDisplay.className = "noahlu-miao";
+    oDisplay.id = "noahlu-miao";
+
+    oHeader = doc.createElement("header");
+    oHeader.innerHTML = "<div>DBQuery Helper</div>" +
+                        " [<a href='#' class='noahlu-miao-link'>展开</a>]";
+
+
+    oStyle = doc.createElement("style");
+    oStyle.innerHTML =
+        ".noahlu-miao{border-radius:2px;background-color:rgba(5,5,5,0.15); padding:3px; position:fixed;top:30px;left:30px;z-index:1000000000;}" +
+        ".noahlu-miao header{text-align:center;}" +
+        ".noahlu-miao-link{font-size:10px; color:red;text-decoration:underline;margin-bottom:3px;text-align:center}" +
+        ".noahlu-miao-timer{color:#fff;font-size:20px;padding:5px;border:1px solid #aaa;background-color:#bbb;border-radius:4px;}" +
+        ".noahlu-score{text-align:center;color:red;}" +
+        ".noahlu-miao-info{text-align: center; margin: 0px; font-size: 12px;}" +
+        ".noahlu-hide{display:none}"
+
+    oScore = doc.createElement('div');
+    oScore.className = 'noahlu-score noahlu-hide';
+    oScore.id = 'noahlu-score';
+
+    oDisplay.appendChild(oHeader);
+    oDisplay.appendChild(oScore);
+
+    doc.head.appendChild(oStyle);
+    doc.body.appendChild(oDisplay);
+
+    // Toggle timer
+    oSwithcer = oHeader.querySelectorAll('a')[0];
+    oSwithcer.innerHTML = "刷新";
+    oSwithcer.addEventListener('click', function(e){
+        e.preventDefault();
+
+        console.log("click!");
+		chrome.runtime.sendMessage({method: "getLocalStorage", key: "height"}, function(response) {
+ 		console.log(response.data);
+ 		var height = response.data; 
+
+		if(!isNaN(height))
+		{
+			console.log("数字");
+            if (height <= 0) 
+            {
+                alert('请先设置大于0的高度！');
+            }
+            else
+            {
+			     modify(height);
+            }
+		}  
+        else
+        {
+           alert('设置的高度值有非法字符！');
+        }
+});        
+    })
+}
+
---
+++ popup.js
@@ -0,0 +1,40 @@
+﻿
+(function(){
+	var $=function(id){return document.getElementById(id);}
+	var Tasks = {
+		show:function(obj){
+			obj.className='';
+			return this;
+		},
+		hide:function(obj){
+			obj.className='hide';
+			return this;
+		},
+		//存储dom
+		$txtHeight:$('txtHeight'),
+		$modifyButton:$('modifyButton'),
+		//指针
+		height:window.localStorage.getItem('height'),
+		//初始化
+		init:function(){
+            //读取保存的值
+			Tasks.$txtHeight.value = Tasks.height;
+
+			/*注册事件*/
+			//回车修改
+			Tasks.$modifyButton.addEventListener('click',function(){
+               window.localStorage.setItem('height', Tasks.$txtHeight.value);
+			}, false);
+
+			Tasks.$txtHeight.addEventListener('keyup', function()
+				{
+                  Tasks.$txtHeight.value=Tasks.$txtHeight.value.replace(/[^\d]/g,'')   
+				},true);
+
+			Tasks.$txtHeight.addEventListener('beforepaste', function(){
+                clipboardData.setData('text',clipboardData.getData('text').replace(/[^\d]/g,''))
+			});
+		}
+	}
+	Tasks.init();
+})();
